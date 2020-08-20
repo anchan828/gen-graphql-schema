c9aafc61d43118fea39b2d209c6e7cf80f188fc6
anchan828 <anchan828@gmail.com>
2020-08-20 12:16:33 +0900

    feat: add mergeOptions

--- packages/merge-schema/src/index.ts
+++ packages/merge-schema/src/index.ts
@@ -2,7 +2,7 @@ import { genOrderTypes, GenOrderTypesOptions } from "@anchan828/gen-graphql-orde
 import { genRelayTypes, GenRelayTypesOptions } from "@anchan828/gen-graphql-relay-schema";
 import { printSchemaWithDirectives } from "@anchan828/gen-graphql-schema-common";
 import { genWhereTypes, GenWhereTypesOptions } from "@anchan828/gen-graphql-where-schema";
-import { mergeTypeDefs } from "@graphql-tools/merge";
+import { Config, mergeTypeDefs } from "@graphql-tools/merge";
 import * as deepmerge from "deepmerge";
 import { buildASTSchema, DocumentNode } from "graphql";
 
@@ -10,6 +10,7 @@ export { printSchemaWithDirectives, toConstanceCase } from "@anchan828/gen-graph
 export const mergeTypes = (
   types: Array<string | DocumentNode>,
   options?: {
+    mergeOptions?: Omit<Partial<Config>, "commentDescriptions">;
     orderOptions?: GenOrderTypesOptions;
     whereOptions?: GenWhereTypesOptions;
     relayOptions?: GenRelayTypesOptions;
@@ -17,7 +18,7 @@ export const mergeTypes = (
 ): string => {
   options = deepmerge({ orderOptions: {}, whereOptions: {}, relayOptions: {} }, options || {});
 
-  let result = genOrderTypes(mergeTypeDefs([...types, `type Query`], {}), options.orderOptions);
+  let result = genOrderTypes(mergeTypeDefs([...types, `type Query`], options.mergeOptions), options.orderOptions);
 
   result = genWhereTypes(result, options.whereOptions);
   result = genRelayTypes(result, options.relayOptions);
